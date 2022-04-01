95d41bbb0dd3f922106a9869ffbd2d0fa58ce4e0
shonnon-zxs <68311986+shonnon-zxs@users.noreply.github.com>
2022-04-01 21:36:30 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,3 +1,40 @@
+    # different lmh to get bias
+    def get_bias(train_dset,eval_dset):
+    # Compute the bias:
+    # The bias here is just the expected score for each answer/question type
+    answer_voc_size = train_dset.num_ans_candidates
+
+    # question_type -> answer -> total score
+    question_type_to_probs = defaultdict(Counter)
+
+    # question_type -> num_occurances
+    question_type_to_count = Counter()
+    for ex in train_dset.entries:
+        ans = ex["anno"]
+        q_type = ans["question_type"]
+        question_type_to_count[q_type] += 1
+        if ans["labels"] is not None:
+            for label, score in zip(ans["labels"], ans["scores"]):
+                question_type_to_probs[q_type][label] += score
+    question_type_to_prob_array = {}
+
+    for q_type, count in question_type_to_count.items():
+        prob_array = np.zeros(answer_voc_size, np.float32)
+        for label, total_score in question_type_to_probs[q_type].items():
+            prob_array[label] += total_score
+        prob_array /= count
+        question_type_to_prob_array[q_type] = prob_array
+
+    # for ds in [train_dset, eval_dset]:
+    for ds in [train_dset]:
+        for ex in ds.entries:
+            q_type = ex["anno"]["question_type"]
+            ex["bias"] = question_type_to_prob_array[q_type]
+            
+            
+            
+            
+            
             # u-u
             if epoch < 12:
                 logits_pos, _= model(q, v, None, None, False)
